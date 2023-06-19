34816f76561f6110bcda127d325df3cc983e348e
EC2 Default User <ec2-user@ip-172-31-39-146.ap-northeast-1.compute.internal>
2023-06-19 09:03:16 +0000

    buf gix

--- samples/python/s3/s3get.py
+++ samples/python/s3/s3get.py
@@ -3,8 +3,8 @@ import boto3
 
 region = 'ap-northeast-1'
 bucket_name = '好きなバケット名'
-file_name = 'download-mycat01.png'
-key = 'sample/mycat01.png'
+file_name = 'download-mycat01.jpeg'
+key = 'sample/mycat01.jpeg'
 
 # s3クライアント作成
 resource = boto3.resource('s3', region_name=region)
